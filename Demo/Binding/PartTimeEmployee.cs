6ad45999bdf9f7763db7a772641027186b4244d2
Mahmoud Mansour <mansour@gmail.com>
2025-08-09 16:28:21 +0300

    Complete 2nd Video - Part 02 When Binding Happens

---
+++ Demo/Binding/PartTimeEmployee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Binding
+{
+    internal class PartTimeEmployee : Employee
+    {
+        #region Properties
+        public decimal HourRate { get; set; }
+        public int CountOfHours { get; set; }
+        #endregion
+
+        #region Constructor
+        public PartTimeEmployee(int id, string? name, int age, decimal hourRate, int countOfHours)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            HourRate = hourRate;
+            CountOfHours = countOfHours;
+        }
+        #endregion
+
+        #region Methods
+        public new void GetEmployeeType()
+        {
+            Console.WriteLine("I am PartTime Employee");
+        }
+        public override void GetEmployeeData()
+        {
+            base.GetEmployeeData();
+            Console.WriteLine($"HourRate = {HourRate} \nCountOfHours = {CountOfHours}");
+        }
+        #endregion
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,29 +1,55 @@
-﻿using Demo.Overriding;
+﻿using Demo.Binding;
+using Demo.Overriding;
 
 namespace Demo
 {
     internal class Program
     {
+        public static void ProcessEmployee(Employee employee)
+        {
+            if (employee is not null)
+            {
+                employee.GetEmployeeType();
+                employee.GetEmployeeData();
+            }
+        }
+        //public static void ProcessEmployee(PartTimeEmployee employee)
+        //{
+        //    if (employee is not null)
+        //    {
+        //        employee.GetEmployeeType();
+        //        employee.GetEmployeeData();
+        //    }
+        //}
         static void Main(string[] args)
         {
             #region Binding
+            #region Emaple 01
             // Reference from Parent [Base] => Object from Child [Derived]
-            TypeA typeA = new TypeB(1, 2);
-            typeA.A = 1;
-            //typeA.B = 2; // Unreachable because we can see only the feild from the Referance 
+            //TypeA typeA = new TypeB(1, 2);
+            //typeA.A = 1;
+            ////typeA.B = 2; // Unreachable because we can see only the feild from the Referance 
 
-            typeA.MyFun01(); // Static Polymorphism
-                             // Static Binding
-                             // Early Binding
-                             // Compile Time Binding
-                             // [Parent based on Reference]
-            
-            typeA.MyFun02(); // Dynamic Polymorphism
-                             // Nonstatic Binding
-                             // Late Binding
-                             // Runtime Binding
-                             // Child based on Object
+            //typeA.MyFun01(); // Static Polymorphism
+            //                 // Static Binding
+            //                 // Early Binding
+            //                 // Compile Time Binding
+            //                 // [Parent based on Reference]
 
+            //typeA.MyFun02(); // Dynamic Polymorphism
+            //                 // Nonstatic Binding
+            //                 // Late Binding
+            //                 // Runtime Binding
+            //                 // Child based on Object
+            #endregion
+
+            #region Example 02
+            //FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(10, "Ahmed", 25, 2000);
+            //ProcessEmployee(fullTimeEmployee);
+
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee(10, "Ahmed", 25, 2, 5);
+            ProcessEmployee(partTimeEmployee);
+            #endregion
             #endregion
         }
     }
