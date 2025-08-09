0008315b50761647fec59c2690dfeced4066bbe4
Mahmoud Mansour <mansour@gmail.com>
2025-08-09 15:51:06 +0300

    Complete 1st Video - Part 01 Binding

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Demo
+﻿using Demo.Overriding;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Binding
+            // Reference from Parent [Base] => Object from Child [Derived]
+            TypeA typeA = new TypeB(1, 2);
+            typeA.A = 1;
+            //typeA.B = 2; // Unreachable because we can see only the feild from the Referance 
+
+            typeA.MyFun01(); // Static Polymorphism
+                             // Static Binding
+                             // Early Binding
+                             // Compile Time Binding
+                             // [Parent based on Reference]
+            
+            typeA.MyFun02(); // Dynamic Polymorphism
+                             // Nonstatic Binding
+                             // Late Binding
+                             // Runtime Binding
+                             // Child based on Object
+
+            #endregion
         }
     }
 }
